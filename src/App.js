e14a645970bfe940a7035abe065560163af4efc7
azeezcodes <81769914+azeezcodes@users.noreply.github.com>
2023-03-20 20:48:17 +0100

    header doen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Header from './components/Header';
+import Divider from "@mui/material/Divider";
 
 function App() {
   return (
     <div className="App">
-    
+      <Header />
+      <Divider sx={{marginTop: "20px"}}/>
     </div>
   );
 }
